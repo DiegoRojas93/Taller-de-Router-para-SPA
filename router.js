13743cc3a7c0484eda1a5aa70908c8db35995019
Diego Rojas <diegorojas431@gmail.com>
2020-06-04 16:24:07 -0500

    Creando la funcion load routes

--- router.js
+++ router.js
@@ -4,6 +4,17 @@ class Router {
     this._loadInitialRoute();
   }
 
+  loadRoute(...urlSegs){
+    const matchedRoute = this.routes.find(urlSegs);
+    const url = `/${urlSegs.join('/')}`;
+
+    history.pushState({},'this works', url);
+
+    const routerOutElm = document.querySelectorAll('[data-router]')[0];
+
+    routerOutElm.innerHTML = matchedRoute.template
+  }
+
   _matchUrlRoute(urlSegs){
     const matchedRoute = this.routes.find(route =>{
       const routePathSegs = route.path.split('/').slice(1)
