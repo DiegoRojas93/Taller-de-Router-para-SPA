d717b001a0d2cef5a72af71f8ce4a31be2a6cdff
Diego Rojas <diegorojas431@gmail.com>
2020-06-04 17:36:11 -0500

    Actualizando nuestro index.html para agregar la nueva funcionalidad de routing

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Talle de creación de Router para Single Page App con JavaScript
+
 Instala el sigiente paquete de forma global en NPM
 ```
 sudo npm install http-server-spa -g
--- router.js
+++ router.js
@@ -1,40 +1,44 @@
 class Router {
-  construrctor(routes){
-    this.routes = routes;
-    this._loadInitialRoute();
+
+  constructor(routes) {
+      this.routes = routes;
+      this._loadInitialRoute();
   }
 
-  loadRoute(...urlSegs){
-    const matchedRoute = this.routes.find(urlSegs);
-    const url = `/${urlSegs.join('/')}`;
+loadRoute(...urlSegs){
 
-    history.pushState({},'this works', url);
+  const matchedRoute = this._matchUrlToRoute(urlSegs);
 
-    const routerOutElm = document.querySelectorAll('[data-router]')[0];
+  const url = `/${urlSegs.join('/')}`;
+  history.pushState({},'this works', url);
 
-    routerOutElm.innerHTML = matchedRoute.template
-  }
+  const routerOutElm = document.querySelectorAll('[data-router]')[0];
+  routerOutElm.innerHTML = matchedRoute.template;
 
-  _matchUrlRoute(urlSegs){
-    const matchedRoute = this.routes.find(route =>{
+}
+
+_matchUrlToRoute(urlSegs){
+  const matchedRoute = this.routes.find(route => {
       const routePathSegs = route.path.split('/').slice(1)
 
       if (routePathSegs.length !== urlSegs.length){
-        return false;
+          return false;
       }
 
       return routePathSegs
-        .every((routePathSegs, i) => routePathSegs === urlSegs.[i]);
-    });
+          .every((routePathSeg, i) => routePathSeg === urlSegs[i]);
+  });
 
-    return matchedRoute;
+  return matchedRoute;
+}
 
+
+_loadInitialRoute() {
+
+  const pathNameSplit = window.location.pathname.split('/');
+  const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1): '';
+
+  this.loadRoute(...pathSegs);
   }
 
-  _loadInitialRoute(){
-    const pathNameSplit = window.location.pathname.split('/');
-    const pathSegs = pathNameSplit.length > 1 ? pathNameSplit(1): '';
-
-    this.loadRoute(...pathSegs);
-  }
 }
